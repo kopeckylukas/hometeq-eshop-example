6142998d62c21ff4f31e530bb0ffcb0a700fcd9e
kopeckylukas <56082570+kopeckylukas@users.noreply.github.com>
2020-03-29 21:15:24 +0100

    Before Going Locally

--- checkout.php
+++ checkout.php
@@ -10,8 +10,9 @@ echo "<body>";
 include ("headfile.html");	     //Calls HTML file that displays header
 include ("detectlogin.php");
 echo "<h4>".$pagename."</h4>";
+/**
 //----------------------- WEBPAGE CODE STARTS HERE -----------------------------
-
+*/
 $currentdatetime = date('Y-m-d H:i:s');
 
 $SQL = "INSERT INTO Orders (userId, orderDateTime, orderTotal)
@@ -28,9 +29,48 @@ if($error == 0){
    echo "<p>Your order has been placed succesfully<p>";
    echo "Your Order Number is :".$orderNumeber['MaxOrderNo'];
 
-}
- echo "done";
+//Table of Ceheckout Pruducts
+  echo "<table style='border: 0px'>";
+  echo "<tr>";
+  echo "<th>Product Name</th>";
+  echo "<th>Price</th>";
+  echo "<th>Quantity</th>";
+  echo "<th>Subtotal</th>";
+  echo "</tr>";
 
+ 	$SQL="SELECT * FROM Product";
+ 	$exeSQL=mysqli_query($conn, $SQL) or die (mysqli_error());
+
+  $total=0;
+  while($arrayp=mysqli_fetch_array($exeSQL)){
+    foreach($_SESSION['basket'] as $ID => $quantity) {
+      if ($arrayp['prodId'] == $ID){
+   	    $subtotal=$quantity*$arrayp['prodPrice'];
+   			echo "<tr>";
+   			echo "<td>".$arrayp['prodName']."</td>";
+   			echo "<td>&pound;".$arrayp['prodPrice']."</td>";
+   			echo "<td>".$quantity."</td>";
+   			echo "<td>&pound;".number_format($subtotal,2)."</td>";
+   			echo "</tr>";
+   			$total+=$subtotal;
+   			$_SESSION['orderTotal']=$total;
+        $SQL4 = "insert into Order_Line (orderNo, prodId, quantityOrdered, subTotal)
+                values (".$orderNumeber['MaxOrderNo'].",".$ID.",".$quantity.",".$subtotal.")";
+                //  values(".$orderNumeber.",".$ID.",".$quantity.",".$subtotal.") ";
+        $exeSQL4 = mysqli_query ($conn, $SQL4);
+   			}
+   		}
+   	}
+   	echo "<tr>";
+   	echo "<th colspan='3'>TOTAL</th>";
+   	echo "<th>&pound;".$total."</th>";
+   	echo "</tr>";
+   	echo "</table>";
+
+    echo "<p> To Log out and leave the system please <a href='logout.php'>Logout</a>";
+}else{
+  echo "There is an Error".$error;
+}
 
 
 //----------------------- WEBPAGE CODE STARTS HERE -----------------------------
